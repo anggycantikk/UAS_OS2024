7d35ec7edb80276dac83f49dc7697d648383a0c5
anggycantikk <anggreynihariminggu2@gmail.com>
2024-12-10 19:11:54 +0900

    first commit

---
+++ konversi.c
@@ -0,0 +1,24 @@
+
+
+void dolar()
+{
+    float rupiah, dolar;
+    printf("Masukkan jumlah rupiah : ");
+    scanf("%f",&rupiah);
+    dolar = rupiah/15943;
+    printf ("\t nilai konversi rupiah ke dolar adalah : $ %f\n", dolar);
+}
+
+void rupiah()
+{
+
+    float rupiah, dolar;
+    printf("Masukkan jumlah dolar : ");
+    scanf("%f",&dolar);
+    rupiah = dolar*15943;
+    printf ("\t nilai konversi rupiah ke dolar adalah : Rp %f\n", dolar);
+}
+
+
+
+
